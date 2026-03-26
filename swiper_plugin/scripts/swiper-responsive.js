92b7ed7ceb763ddfc88d7df4b898e19166dfac2c
dptjr1020 <dptjr1020@gmail.com>
2026-03-26 14:22:00 +0900

    Swiper&sva resposible

---
+++ swiper_plugin/scripts/swiper-responsive.js
@@ -0,0 +1,80 @@
+//변
+const best = document.querySelector('.best_swiper')
+const season = document.querySelectorAll('.season_wrap .swiper')
+const season_title = document.querySelectorAll('.season_wrap .tab_title a')
+console.log(best, season, season_title)
+
+//Swiper연결
+const bestSwiper = new Swiper(best, {
+    //반응형 Swiper 제작 시 모바일기준부터 계삭하면서 시작
+    slidesPerView:2, //모바일에서 보이는 개수
+    spaceBetween:20,
+    //반응형 옵션 breakpoints (사용자 디바이스 너비 기준)
+    //반응형 너비 평균 640 / 768 / 1024 (디자인에 따라 수정,추가 가능)
+    breakpoints:{
+        640:{},//640이상일 때
+        768:{
+            slidesPerView:3,
+        },//768이상일 때
+        1024:{
+            slidesPerView:4,
+        },//1024이상일 때
+    },
+    navigation:{
+        prevEl:'.contents .prev',
+        nextEl:'.contents .next',
+    },
+})
+const nikeSwiper = new Swiper(season[0], {
+    slidesPerView:2,
+    spaceBetween:20,
+    breakpoints:{
+        640:{},
+        768:{
+            slidesPerView:3,
+        },
+        1024:{
+            slidesPerView:4,
+        },
+    },
+})
+const converseSwiper = new Swiper(season[1], {
+    slidesPerView:2,
+    spaceBetween:20,
+    breakpoints:{
+        640:{},
+        768:{
+            slidesPerView:3,
+        },
+        1024:{
+            slidesPerView:4,
+        },
+    },
+})
+
+//Tab제목 클릭 시 관련 내용 JS
+//1. 컨버스 숨기기
+season[1].style.display = 'none';
+//2. 컨버스탭제목 클릭(active변경) -> 컨버스 보이기, 나이키 숨기기
+season_title[1].addEventListener('click',function(e){
+    e.preventDefault();
+    season_title[1].classList.add('active');
+    season_title[0].classList.remove('active');
+    season[1].style.display = 'block';
+    season[0].style.display = 'none';
+})
+season[0].style.display = 'none';
+//3. 나이키탭제목 클릭(active변경) -> 컨버스 숨기기, 나이키 보이기
+season_title[0].addEventListener('click',function(e){
+    e.preventDefault();
+    season_title[0].classList.add('active');
+    season_title[1].classList.remove('active');
+    season[0].style.display = 'block';
+    season[1].style.display = 'none';
+})
+
+//a변수를 클릭이벤트함수로 사용 시 a의 다른 페이지 이동기능때문에 스크롤이 위로 올라가는 문제 해결법
+// HTML해결법) <a href="javascript:void(0)"> 태그가 많으면 불편할 수 있음.
+//JS해결법) a태그.addEventListener('클릭',function(){});명령기준
+//1. 위 명령 function(매개변수) 입력
+//2. 매개변수.preventDefault(); //a의 기본기능 막기를 의미함.
